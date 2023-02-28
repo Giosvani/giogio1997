a999de230d2033b466786d688068c652bf928276
unknown <qaldanig97@icloud.com>
2023-02-28 18:35:09 +0400

    Add new VS solution

---
+++ giogio/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+
+namespace giogio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Enter some text");
+            var someWord = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(someWord + "Added value");
+            Console.ResetColor();
+             
+
+            Console.WriteLine("    /|");    
+            Console.WriteLine("   / |" );
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");
+
+              
+                
+                
+
+
+            {
+              
+            }
+
+
+                
+
+             
+                
+
+       
+        }
+    }
+}
